5a7b295b47187e0c603508f6eea378070a949bcc
Jay Moker <dadamien78@gmail.com>
2024-08-13 11:19:01 +0200

    Removed new keyword from libft

--- libft/src/ft_lstadd_back.c
+++ libft/src/ft_lstadd_back.c
@@ -6,21 +6,21 @@
 /*   By: dbaladro <dbaladro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:48:30 by dbaladro          #+#    #+#             */
-/*   Updated: 2024/01/17 12:32:59 by dbaladro         ###   ########.fr       */
+/*   Updated: 2024/08/13 11:17:54 by dbaladro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-void	ft_lstadd_back(t_list **alst, t_list *new)
+void	ft_lstadd_back(t_list **alst, t_list *new_elem)
 {
 	t_list	*last;
 
 	if (!(*alst))
-		*alst = new;
+		*alst = new_elem;
 	else
 	{
 		last = ft_lstlast(*alst);
-		last->next = new;
+		last->next = new_elem;
 	}
 }
--- libft/src/ft_lstadd_front.c
+++ libft/src/ft_lstadd_front.c
@@ -6,14 +6,14 @@
 /*   By: dbaladro <dbaladro@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/11/07 09:48:34 by dbaladro          #+#    #+#             */
-/*   Updated: 2024/01/17 12:32:59 by dbaladro         ###   ########.fr       */
+/*   Updated: 2024/08/13 11:18:18 by dbaladro         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/libft.h"
 
-void	ft_lstadd_front(t_list **alst, t_list *new)
+void	ft_lstadd_front(t_list **alst, t_list *new_elem)
 {
-	new->next = *alst;
-	*alst = new;
+	new_elem->next = *alst;
+	*alst = new_elem;
 }
