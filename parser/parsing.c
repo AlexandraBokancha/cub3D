c2e09208bb139874b0534a859fb16e1e11200877
albokanc <albokanc@student.42.fr>
2024-08-20 18:23:01 +0200

    parsing and filling map

--- parser/parsing.c
+++ parser/parsing.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   parsing.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alexandra <alexandra@student.42.fr>        +#+  +:+       +#+        */
+/*   By: albokanc <albokanc@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/18 17:15:51 by alexandra         #+#    #+#             */
-/*   Updated: 2024/08/19 20:28:20 by alexandra        ###   ########.fr       */
+/*   Updated: 2024/08/20 17:31:29 by albokanc         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -56,6 +56,124 @@ int parsing_colors(char *color)
     return (1);    
 }
 
+int	check_first_last(char *line)
+{
+	int i;
+
+	i = 0;
+	while(line[i] && line[i] != '\n')
+	{	
+		if (line[i] != '1')
+		{	
+			write(2, "Error. Map is not closed\n", 26);
+			return (0);
+			break;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	check_borders(char *line)
+{
+	//printf("%zu\n", ft_strlen(line));
+	if (line[0] != '1')
+	{
+		write(2, "Error. Map is not closed\n", 26);
+			return (0);
+	}
+	if (line[ft_strlen(line) - 2] != '1')
+	{
+		write(2, "Error. Map is not closed\n", 26);
+			return (0);
+	}
+	return (1);
+}
+
+int	is_wall(char *line, int last, int i)
+{
+	if (i == 0 || i == last - 1)
+	{
+		if (!check_first_last(line))
+			return (0);
+	}
+	else
+	{
+		if (!check_borders(line))
+			return (0);
+	}
+	return (1);
+}
+
+int	is_closed(char **map, int height)
+{
+	int	i;
+
+	i = 0;
+	while (map[i])
+	{
+		if (!is_wall(map[i], height, i))
+			return (0);
+		i++;
+	}
+	return (1);
+}
+
+void	fill_spaces(char **map, int height)
+{
+	int i;
+	int j;
+
+	i = 0;
+	while (i < height)
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] == ' ')
+				map[i][j] = '1';
+			j++;
+		}
+		i++;
+	}
+}
+
+int is_valid_chars(char **map, int height)
+{
+	int	i;
+	int	j;
+
+	i = 0;
+	while (i < height)
+	{
+		j = 0;
+		while (map[i][j])
+		{
+			if (map[i][j] != '1' || map[i][j] != '0' \
+				|| map[i][j] != 'N' || map[i][j] != 'S' \
+				|| map[i][j] != 'E' || map[i][j] != 'W')
+			{
+				write(2, "Error. Map is not closed\n", 26);
+				return (0);
+				break ;
+			}
+			j++;
+		}
+		i++;
+	}
+	return (1);
+}
+
+int	parsing_map(t_map_info *map_info)
+{
+	fill_spaces(map_info->map2d, map_info->map2_height);
+	if (!is_valid_chars(map_info->map2d, map_info->map2_height))
+		return (1);
+	if (!is_closed(map_info->map2d, map_info->map2_height))
+		return (1);
+	return (0);	
+};
+
 int parsing(t_data *data)
 {
     int i;
@@ -72,6 +190,8 @@ int parsing(t_data *data)
     }
     if (!parsing_colors(data->colors.f_color))
         return (1);
-    //parsing_map();
+	//save colors in rgb ??
+    if (parsing_map(&data->map_info))
+		return (1);
     return (0);
 }
